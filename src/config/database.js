3fb64743cc9485438ed6d2e3b314fb6c11490896
stitipragyan barik <stitipragyanbarik@email.com>
2025-11-02 23:32:47 +0530

    Ignore temp folder for uploaded videos

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      tls: true,
+      tlsAllowInvalidCertificates: false,
+      serverSelectionTimeoutMS: 5000, // 5 seconds
+    });
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
